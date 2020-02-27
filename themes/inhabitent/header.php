89f01f491aa500bb7cbd6fbcc820878213605a0a
unknown <calyston.14@gmail.com>
2020-02-27 15:54:42 -0500

    feat: nav bar is green except for home and about pages

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -8,11 +8,42 @@
 </head>
 <body <?php body_class();?>>
 <!-- <h1><?php bloginfo('name');?></h1> -->
-<header class="white-nav">
-  <a href="/inhabitent"><img id="logo-small-white" src="<?php echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-tent-white.svg"></a>
-  <nav>
+<header>
+  <!-- <nav class="white-nav">
+  <a href="/inhabitent"><img id="logo-small-white" src="<?php //echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-tent-white.svg"></a>
+    <?php //wp_nav_menu(array(
+      //'theme_location' => 'main'
+  //  )) ;?>
+  </nav> -->
+
+  
+
+<!-- <nav class="<?php //echo is_page(array('About', 'Home')) ? 'transparent' : 'opaque' ;?>">
+<?php// wp_nav_menu(array(
+   // 'theme_location' => 'main'
+//)) ;?>
+</nav> -->
+
+<?php if( is_page(array('About', 'Home')) ){ ?>
+    <nav class="white-nav">
+    <a href="/inhabitent"><img id="logo-small-white" src="<?php echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-tent-white.svg"></a>
+    
     <?php wp_nav_menu(array(
       'theme_location' => 'main'
     )) ;?>
-  </nav>
+    </nav>
+
+    <?php } else { ?>
+
+    <nav class="green-nav">
+    <a href="/inhabitent"><img id="logo-small-green" src="<?php echo get_template_directory_uri();?>/assets/images/logos/inhabitent-logo-tent.svg"></a>
+    <?php wp_nav_menu(array(
+      'theme_location' => 'main'
+    )) ;?>
+    </nav>
+
+    <?php } ?>
+
+<!-- </nav> -->
+
 </header>
