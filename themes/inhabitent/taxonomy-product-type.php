5858e95562b5840d4db3a2f44709a348fa225179
unknown <calyston.14@gmail.com>
2020-02-28 15:41:30 -0500

    stye: single product page styling fixed

--- themes/inhabitent/taxonomy-product-type.php
+++ themes/inhabitent/taxonomy-product-type.php
@@ -1,23 +1,37 @@
 <?php get_header(); ?>
+<div id="empty-div"></div>
+
+<div id="shop-header">
+  <h2 class="shop-title">Change the title!</h2>
+</div>
+
+<section class="shop-grid">
 
 <?php if( have_posts() ) :
 
 //The WordPress Loop: loads post content 
     while( have_posts() ) :
         the_post(); ?>
+    <div class="shop-item">
+    <a href="<?php the_permalink();?>"><img src=<?php the_post_thumbnail();?></a>
     
-    <h2><?php the_title(); ?></h2>
-    <?php the_post_thumbnail();?>
-    <h3><?php the_permalink();?></h3>
-    <?php the_content(); ?>
-    
+    <span class="archive-product-info">
+      <p class="archive-product-title"><?php the_title(); ?></p>
+      <p class="archive-product-price"><?php echo '$'.get_field('price');?></p>
+    </span>
+    <!-- <h3><?php the_permalink();?></h3>
+    <?php the_content(); ?> -->
+    </div>
+
     <!-- Loop ends -->
     <?php endwhile;?>
 
     <?php the_posts_navigation();?>
+    <?php wp_reset_postdata(); ?>
 
 <?php else : ?>
         <p>No posts found</p>
 <?php endif;?>
+</section>
     
 <?php get_footer();?>
