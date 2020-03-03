9e3078195e53cb236b7a70ef5ede86d2410e56ec
unknown <calyston.14@gmail.com>
2020-03-02 22:24:31 -0500

    style: footer items better positioned

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -1,8 +1,11 @@
 <footer>
   <?php wp_footer();?>
-  <?php dynamic_sidebar('contact-info'); ?>
-  <?php dynamic_sidebar('business-hours'); ?>
+  <div id="footer-info">
+    <?php dynamic_sidebar('contact-info'); ?>
+    <?php dynamic_sidebar('business-hours'); ?>
+  </div>
   <span>COPYRIGHT © 2020 INHABITENT</span>
+  <img src='<?php echo get_template_directory_uri() . "/assets/images/logos/inhabitent-logo-text.svg";?>'>
 </footer>
 </body>
 </html>
